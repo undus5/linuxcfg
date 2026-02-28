6966ec23f31253fcb72c4c8972365ae2a736d296
undus5 <contact@undus.net>
2026-02-28 11:20:38 +0800

    fix wlstart typo

--- sbin/wlstart.sh
+++ sbin/wlstart.sh
@@ -2,17 +2,17 @@
 
 chkcmd() { command -v "${@}" &>/dev/null; }
 chksrv() { pidof "${@}" &>/dev/null; }
-bkr() { nohup "${@}" &>/dev/null & }
+bgr() { nohup "${@}" &>/dev/null & }
 
-chkcmd kanshi && ! chksrv kanshi && bkr hanshi
-chkcmd fcitx5 && ! chksrv fcitx5 && bkr fcitx5 -d -r
+chkcmd kanshi && ! chksrv kanshi && bgr kanshi
+chkcmd fcitx5 && ! chksrv fcitx5 && bgr fcitx5 -d -r
 
 polkit_name=polkit-mate-authentication-agent-1
 polkit_arch=/usr/lib/mate-polkit/${polkit_name}
 polkit_fedora=/usr/libexec/${polkit_name}
 [[ -f "${polkit_arch}" ]] && polkit_exec="${polkit_arch}"
 [[ -f "${polkit_fedora}" ]] && polkit_exec="${polkit_fedora}"
-chkcmd ${polkit_exec} && ! chksrv ${polkit_name} && bkr ${polkit_exec}
+chkcmd ${polkit_exec} && ! chksrv ${polkit_name} && bgr ${polkit_exec}
 
 chkcmd evars.sh && source $(which evars.sh)
 if [[ -d "${bgdir}" ]]; then
