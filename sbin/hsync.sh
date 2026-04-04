3194a1ab326ea56015370ad3f0c6f084cb16291f
undus5 <contact@undus.net>
2026-04-04 15:28:34 +0800

    improve hsync.sh

--- sbin/hsync.sh
+++ sbin/hsync.sh
@@ -2,24 +2,37 @@
 
 errf() { printf "${@}" >&2 && exit 1; }
 
-usage="Usage: $(basename ${0}) <a|b|d|s|t> <hostname>\n"
-hostn="${2}"
+usage="Usage: $(basename ${0}) <hostname> <a|b|d|t|s>\n"
+hostn="${1}"
+opts="${2}"
 
-case "${1}" in
-    a|b|d|t)
-        [[ "${1}" =~ ^[abdt]$ ]] || errf "${usage}"
-        dir=~/${1}/
-        hostip=$(lanip.sh ${hostn})
-        [[ -n "${hostip}" ]] || errf "invalid hostname\n"
-        rsync -aP --del ${dir} ${hostip}:${dir}
-        ;;
+dsync() {
+    # [[ "${1}" =~ ^[abdt]$ ]] || errf "${usage}"
+    dir=~/${1}/
+    hostip=$(lanip.sh ${hostn})
+    [[ -n "${hostip}" ]] || errf "invalid hostname\n"
+    rsync -aP --del ${dir} ${hostip}:${dir}
+}
+
+case "${opts}" in
     s)
         hostip=$(lanip.sh ${hostn})
         [[ -n "${hostip}" ]] || errf "invalid hostname\n"
         ssh ${hostip}
         ;;
     *)
-        errf "${usage}"
+        # empty check
+        [[ -n "${opts}" ]] || errf "${usage}"
+        # remove duplicates
+        opts=$(echo "${opts}" | grep -o . | sort -u | tr -d '\n')
+        # validate opts
+        [[ "${opts}" =~ ^[abdt]+$ ]] || errf "${usage}"
+        # split opts
+        for (( i=0; i<${#opts}; i++ )); do
+            d="${opts:i:1}"
+            [[ "${d}" =~ ^[abdt]$ ]] || errf "${usage}"
+            dsync "${d}"
+        done
         ;;
 esac
 
